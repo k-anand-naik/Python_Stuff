dc93363752af7cbb2a555aa9e907901eb9946782
K-Anand-Naik <91389695+K-Anand-Naik@users.noreply.github.com>
2022-02-01 00:34:45 +0530

    Add files via upload

---
+++ Python_Brilliant/Drawing many_pointed stars/reading and running code.py
@@ -0,0 +1,51 @@
+#print() command
+print("Hello world!")
+print("Anand")
+
+#libraries --> turtle
+#from turtle import *
+#forward(300) # turtle moves 300 pixels forward
+#bye()
+
+#from turtle import *
+
+#forward(200) # how many pixels to go forward
+#left(90)  #how much angle to turn
+#forward(100)
+
+#bye()
+
+#from turtle import *
+
+#forward(200)
+#left(60) #left(90)
+#forward(200)
+#left(60) #left(90)
+#forward(200)
+#left(60) #left(90)
+#forward(200)
+
+#bye()
+
+from turtle import *
+
+#forward(200)
+
+# Make a diameter 200 semicircle, curving down
+for i in range(200): #range(100) prints only semi-circle
+    forward(1.1416)
+    right(1.8)
+for j in range(200):    
+    forward(2.1416)
+    left(1.8)
+for j in range(200):    
+    forward(3.1416)
+    right(1.8)
+for j in range(200):    
+    forward(4.1416)
+    left(1.8)
+
+
+#forward(200)
+
+bye()
